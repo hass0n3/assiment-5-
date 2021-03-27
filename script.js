2bda51a5b3dd8d0363e64613ef0e2bd87d12e0ac
hass0n3 <hassen.alfaris@ocsbstudent.ca>
2021-03-27 19:19:16 +0000

    100

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function MyFunction0() {
-var x  = document.getElementById("Nb").value ;
-x = parseInt(x,10);
+                          var x  = document.getElementById("Nb").value ;
+                          x = parseInt(x,10);
 
-var result  = (13.4)*x ;
-document.getElementById("demo1").innerHTML = "Area =" + result;
+                          var result  = (13.4)*x ;
+                          document.getElementById("demo1").innerHTML = "Area =" + result;
 }
